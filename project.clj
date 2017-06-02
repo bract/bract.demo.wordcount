dd7a97a248251ce3d6268f1f3bc9ccf009753735
Shantanu Kumar <kumar.shantanu@gmail.com>
2017-06-02 10:34:52 +0530

    use the main entrypoint from bract.cli

--- project.clj
+++ project.clj
@@ -11,7 +11,7 @@
   :dependencies [[org.clojure/clojure "1.8.0"]
                  [bract/bract.core "0.2.0-SNAPSHOT"]
                  [bract/bract.cli  "0.2.0-SNAPSHOT"]]
-  :main ^:skip-aot bract.core.main
+  :main ^:skip-aot bract.cli.main
   :target-path "target/%s"
   :profiles {:dev {:dependencies [[org.clojure/tools.namespace "0.2.11"]]
                    :source-paths ["dev"]}
