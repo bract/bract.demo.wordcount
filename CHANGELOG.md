07974fd41dd319e55d91d86dca09cc919bf9ebb7
Shantanu Kumar <kumar.shantanu@gmail.com>
2017-06-02 10:26:08 +0530

    kick off 0.2.0 development

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -1,14 +1,21 @@
 # TODO and Change Log
 
 
+## [WIP] 0.2.0 / 2017-June-??
+
+### Changed
+- Upgrade to `[bract.core "0.2.0"]`
+  - [TODO] Use the GA version before release
+- Use `[bract.cli "0.2.0"]`
+  - [TODO] Use the GA version before release
+
+
 ## 0.1.0 / 2017-April-25
 
 ### Added
-
 - Implement word count
 - Exclude configured stop words
 - Automated tests
   - Using example input
   - Using external config
 - REPL based development helpers
-
--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject bract/bract.demo.wordcount "0.1.0"
+(defproject bract/bract.demo.wordcount "0.2.0-SNAPSHOT"
   :description "Bract sample application for counting words in a text"
   :url "https://github.com/bract/bract.demo.wordcount"
   :license {:name "Eclipse Public License"
@@ -9,7 +9,8 @@
   :min-lein-version "2.7.1"
   :pedantic? :abort
   :dependencies [[org.clojure/clojure "1.8.0"]
-                 [bract/bract.core "0.1.0"]]
+                 [bract/bract.core "0.2.0-SNAPSHOT"]
+                 [bract/bract.cli  "0.2.0-SNAPSHOT"]]
   :main ^:skip-aot bract.core.main
   :target-path "target/%s"
   :profiles {:dev {:dependencies [[org.clojure/tools.namespace "0.2.11"]]
