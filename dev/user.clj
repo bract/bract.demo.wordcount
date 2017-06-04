0726be44d2feb31b36d3011426a3594019075831
Shantanu Kumar <kumar.shantanu@gmail.com>
2017-06-04 12:03:35 +0530

    add alias to the 'verbose' function for the REPL

--- dev/user.clj
+++ dev/user.clj
@@ -1,4 +1,4 @@
 (ns user
   (:require
     [bract.core.dev   :refer [start stop]]
-    [bract.dev.reload :refer [go reinit reset restart]]))
+    [bract.dev.reload :refer [go reinit reset restart verbose]]))
