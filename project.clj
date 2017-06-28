67afb897e2899cfb9b99086f98a854c49dde78a6
Shantanu Kumar <kumar.shantanu@gmail.com>
2017-06-29 00:28:24 +0530

    use bract 0.3.1-SNAPSHOT components

--- project.clj
+++ project.clj
@@ -9,9 +9,9 @@
   :min-lein-version "2.7.1"
   :pedantic? :abort
   :dependencies [[org.clojure/clojure "1.8.0"]
-                 [bract/bract.cli     "0.3.0"]]
+                 [bract/bract.cli     "0.3.1-SNAPSHOT"]]
   :target-path "target/%s"
-  :profiles {:dev {:dependencies [[bract/bract.dev "0.3.0"]]
+  :profiles {:dev {:dependencies [[bract/bract.dev "0.3.1-SNAPSHOT"]]
                    :source-paths ["dev"]}
              :uberjar {:aot [bract.cli.main]
                        :main ^:skip-aot bract.cli.main}})
