3c84a41dc6d56243af7536c4160247fbb09a16d0
Shantanu Kumar <kumar.shantanu@gmail.com>
2018-03-25 10:59:41 +0530

    upgrade Bract modules to version 0.6.0-alpha4

--- project.clj
+++ project.clj
@@ -9,9 +9,9 @@
   :min-lein-version "2.7.1"
   :pedantic? :warn
   :dependencies [[org.clojure/clojure "1.9.0"]
-                 [bract/bract.cli     "0.6.0-alpha3"]]
+                 [bract/bract.cli     "0.6.0-alpha4"]]
   :target-path "target/%s"
-  :profiles {:dev {:dependencies [[bract/bract.dev "0.6.0-alpha3"]]
+  :profiles {:dev {:dependencies [[bract/bract.dev "0.6.0-alpha4"]]
                    :source-paths ["dev"]}
              :uberjar {:aot [bract.core.main]
                        :main ^:skip-aot bract.core.main
