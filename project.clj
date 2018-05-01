972175e6a3823171773f68a0265873af7df1cca7
Shantanu Kumar <kumar.shantanu@gmail.com>
2018-05-01 22:49:17 +0530

    use bract 0.6.0-beta2 components

--- project.clj
+++ project.clj
@@ -9,9 +9,9 @@
   :min-lein-version "2.7.1"
   :pedantic? :warn
   :dependencies [[org.clojure/clojure "1.9.0"]
-                 [bract/bract.cli     "0.6.0-beta1"]]
+                 [bract/bract.cli     "0.6.0-beta2"]]
   :target-path "target/%s"
-  :profiles {:dev {:dependencies [[bract/bract.dev "0.6.0-beta1"]]
+  :profiles {:dev {:dependencies [[bract/bract.dev "0.6.0-beta2"]]
                    :source-paths ["dev"]}
              :uberjar {:aot [bract.core.main]
                        :main ^:skip-aot bract.core.main
